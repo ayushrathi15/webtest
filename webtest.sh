8a798879dd4165fa5f5b462b0fc61e3fd91df5b7
Ayush Rathi <ayushrathi15@gmail.com>
2019-11-12 13:34:11 +0530

    new pull from wordpress 12Nov1334

--- webtest.sh
+++ webtest.sh
@@ -2,12 +2,8 @@
 git remote rm origin
 git remote add origin https://github.com/ayushrathi15/webtest.git
 git pull origin master
-branchstatus=$(git ls-remote --heads https://github.com/ayushrathi15/webtest.git new12nov | wc -l)
-if [ $branchstatus == 1 ];
-then 
-	git branch -D new$(date +%d%b)
-else
-	git checkout -b new$(date +%d%b)
-fi
+git checkout -b newwppush$(date +%d%b%H%M)
+#run wordpress script here
 git add .
-git commit -m newpull
+git commit -m "new pull from wordpress $(date +%d%b%H%M)"
+git push --set-upstream origin newwppush$(date +%d%b%H%M)
