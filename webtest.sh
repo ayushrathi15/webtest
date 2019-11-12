8c6cf08a7164f3deabe87bc747e0d1a791298c8a
ayushrathi <ayushrathi15@gmail.com>
2019-11-12 14:05:49 +0530

    new pull from wordpress 12Nov134904

--- webtest.sh
+++ webtest.sh
@@ -2,7 +2,7 @@
 git remote rm origin
 git remote add origin https://github.com/ayushrathi15/webtest.git
 git pull origin master
-a ="$(date +%d%b%H%M%S)"
+a="$(date +%d%b%H%M%S)"
 git checkout -b newwppush$a
 #run wordpress script here
 git add .
